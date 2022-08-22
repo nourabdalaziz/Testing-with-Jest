4ddb464392ebc7d1d9fe0c57154db125aec1a73b
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-08-22 22:50:04 +0300

    reduce function and its testing

--- Implementations/reduce.js
+++ Implementations/reduce.js
@@ -1,4 +1,21 @@
 let reduce = function (array, callbackFun, initialVal) {
+  if (!array && !callbackFun && !initialVal) {
+    return new Error("No Arguments supported");
+  } else if (!(array instanceof Array)) {
+    return new Error(`${array} is not an array`);
+  } else if (array && !callbackFun) {
+    return new Error("Undefined is not a function");
+  } else if (array.length == 0 && !initialVal) {
+    return new Error("Reduce of empty array with no initial value");
+  } else if (array.length == 0 && initialVal) {
+    return initialVal;
+  }
+
+  if (!initialVal) {
+    initialVal = array[0];
+    array.shift();
+  }
+
   let result = initialVal;
   for (element of array) {
     result = callbackFun(result, element);
@@ -17,13 +34,6 @@ const theSum = (...args) => {
 const flatten = (previousValue, currentValue) =>
   previousValue.concat(currentValue);
 
-const groupObjects = (acc, obj, property) => {
-  const key = obj[property];
-  acc[key] ??= [];
-  acc[key].push(obj);
-  return acc;
-};
-
 // let arr = [1, 9, 20, 40, 5, 25];
 // let x = reduceImplementation(arr, theSum,0);
 // console.log(x);
@@ -35,4 +45,8 @@ const groupObjects = (acc, obj, property) => {
 // ];
 // let x = reduceImplementation(arr, flatten,[]);
 // console.log(x);
+
+// const flattened = [].reduce(flatten, [1]);
+// console.log(flattened);
+
 module.exports = reduce;
--- __tests__/reduce.test.js
+++ __tests__/reduce.test.js
@@ -1,1 +1,70 @@
 const reduce = require("../Implementations/reduce");
+
+test("No arguments sent", () => {
+  expect(reduce()).toEqual(new Error("No Arguments supported"));
+});
+
+test("send an array only", () => {
+  let testArr = [1, 2, 3];
+  expect(reduce(testArr)).toEqual(new Error("Undefined is not a function"));
+});
+
+test("send a function only", () => {
+  let testFun = function () {};
+  expect(reduce(testFun)).toEqual(new Error(`${testFun} is not an array`));
+});
+
+test("send correct data, an array and a function with initial value", () => {
+  const flatten = (previousValue, currentValue) =>
+    previousValue.concat(currentValue);
+
+  let arr = [
+    [0, 1],
+    [2, 3],
+    [4, 5],
+  ];
+  expect(reduce(arr, flatten, [])).toEqual([0, 1, 2, 3, 4, 5]);
+});
+
+test("send correct data, an array and a function without initial value", () => {
+  const flatten = (previousValue, currentValue) =>
+    previousValue.concat(currentValue);
+
+  let arr = [
+    [0, 1],
+    [2, 3],
+    [4, 5],
+  ];
+  expect(reduce(arr, flatten)).toEqual([0, 1, 2, 3, 4, 5]);
+});
+
+test("send correct data, an array and a function without initial value", () => {
+  const theSum = (...args) => {
+    let sum = 0;
+
+    for (let arg of args) sum += arg;
+
+    return sum;
+  };
+
+  let nums = [1, 2, 3];
+  expect(reduce(nums, theSum)).toBe(6);
+});
+
+test("send an object instead of an array", () => {
+  let testF = function () {};
+  let obj = { name: "nour", id: 123456 };
+  expect(reduce(obj, testF)).toEqual(new Error(`${obj} is not an array`));
+});
+
+test("send empty array with a function and without an initial value", () => {
+  let tester = function () {};
+  expect(reduce([], tester)).toEqual(
+    new Error("Reduce of empty array with no initial value")
+  );
+});
+
+test("send empty array with a function and with an initial value", () => {
+  let testerFn = function () {};
+  expect(reduce([], testerFn, [1])).toEqual([1]);
+});
