41479d77e4ca3725a58eb2931686df459a7fd6f7
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-08-23 21:09:16 +0300

    update filter function

--- Implementations/filter.js
+++ Implementations/filter.js
@@ -1,4 +1,12 @@
 let filter = (array, callbackFun) => {
+  if (!array && !callbackFun) {
+    return new Error("No Arguments supported");
+  } else if (!(array instanceof Array)) {
+    return new Error(`${array} is not an array`);
+  } else if (array && !callbackFun) {
+    return new Error("Undefined is not a function");
+  }
+
   let result = [];
 
   for (element of array) {
@@ -9,35 +17,4 @@ let filter = (array, callbackFun) => {
 
   return result;
 };
-
-function filterByID(item) {
-  if (Number.isFinite(item.id) && item.id !== 0) {
-    return true;
-  }
-  invalidEntries++;
-  return false;
-}
-
-let users = [
-  { id: 1, name: "John" },
-  { id: 2, name: "Pete" },
-  { id: 3, name: "Mary" },
-];
-
-console.log(filter(users, (item) => item.id < 3));
-
-const arr = [
-  { id: 15 },
-  { id: -1 },
-  { id: 0 },
-  { id: 3 },
-  { id: 12.2 },
-  {},
-  { id: null },
-  { id: NaN },
-  { id: "undefined" },
-];
-
-let invalidEntries = 0;
-console.log(filter(arr, filterByID));
-console.log(invalidEntries);
+module.exports = filter;
