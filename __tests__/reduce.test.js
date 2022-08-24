4e7d9b73a4234c52df6f2fd4f81e5b980e3e0234
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-08-24 22:30:50 +0300

    refactor reduce-function testing

--- __tests__/reduce.test.js
+++ __tests__/reduce.test.js
@@ -1,17 +1,23 @@
 const reduce = require("../Implementations/reduce");
 
 test("No arguments sent", () => {
-  expect(reduce()).toEqual(new Error("No Arguments supported"));
+  expect(() => {
+    reduce();
+  }).toThrow("missing argument");
 });
 
 test("send an array only", () => {
   let testArr = [1, 2, 3];
-  expect(reduce(testArr)).toEqual(new Error("Undefined is not a function"));
+  expect(() => {
+    reduce(testArr);
+  }).toThrow("undefined is not a function");
 });
 
 test("send a function only", () => {
-  let testFun = function () {};
-  expect(reduce(testFun)).toEqual(new Error(`${testFun} is not an array`));
+  const testFun = () => {};
+  expect(() => {
+    reduce(testFun);
+  }).toThrow(`${testFun} is not an array`);
 });
 
 test("send correct data, an array and a function with initial value", () => {
@@ -64,20 +70,23 @@ test("send correct data, an array and a function with initial value", () => {
   expect(reduce(arrNums, findSum, 0)).toBe(100);
 });
 
-test("send an object instead of an array", () => {
-  let testF = function () {};
-  let obj = { name: "nour", id: 123456 };
-  expect(reduce(obj, testF)).toEqual(new Error(`${obj} is not an array`));
+test("send an object instead of an array ", () => {
+  const testF = () => {};
+
+  let obj = { name: "nour", id: 123 };
+  expect(() => {
+    reduce(obj, testF);
+  }).toThrow(`${obj} is not an array`);
 });
 
 test("send empty array with a function and without an initial value", () => {
-  let tester = function () {};
-  expect(reduce([], tester)).toEqual(
-    new Error("Reduce of empty array with no initial value")
-  );
+  const tester = () => {};
+  expect(() => {
+    reduce([], tester);
+  }).toThrow("Reduce of empty array with no initial value");
 });
 
 test("send empty array with a function and with an initial value", () => {
-  let testerFn = function () {};
-  expect(reduce([], testerFn, [1])).toEqual([1]);
+  const tester = () => {};
+  expect(reduce([], tester, [1])).toEqual([1]);
 });
