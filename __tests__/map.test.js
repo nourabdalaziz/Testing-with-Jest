d821c3394e017ef9f6b573896439067378cfb226
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-08-24 22:12:53 +0300

    refactor map testing

--- __tests__/map.test.js
+++ __tests__/map.test.js
@@ -1,51 +1,60 @@
 const map = require("../Implementations/map");
 
 test("No arguments sent", () => {
-  expect(map()).toEqual(new Error("No Arguments supported"));
-});
-
-test("Null arguments", () => {
-  expect(map(null, null)).toEqual(new Error("No Arguments supported"));
+  expect(() => {
+    map();
+  }).toThrow("missing argument");
 });
 
 test("send an array only", () => {
   let testArr = [1, 2, 3];
-  expect(map(testArr)).toEqual(new Error("Undefined is not a function"));
+  expect(() => {
+    map(testArr);
+  }).toThrow("undefined is not a function");
 });
 
 test("send a function only", () => {
-  let testFun = function () {};
-  expect(map(testFun)).toEqual(new Error(`${testFun} is not an array`));
+  const testFun = (item) => item ** 2;
+  expect(() => {
+    map(testFun);
+  }).toThrow(`${testFun} is not an array`);
 });
 
 test("send correct data, an array and a function", () => {
-  let testF = (item) => item ** 2;
+  const testF = (item) => item ** 2;
   let testA = [1, 2, 3];
   expect(map(testA, testF)).toEqual([1, 4, 9]);
 });
 
-test("send an object ", () => {
-  let testF = (item) => item ** 2;
+test("send an object instead of an array ", () => {
+  const testF = (item) => item ** 2;
   let obj = { name: "nour", id: 123456 };
-  expect(map(obj, testF)).toEqual(new Error(`${obj} is not an array`));
+  expect(() => {
+    map(obj, testF);
+  }).toThrow(`${obj} is not an array`);
 });
 
 test("send a number instead of an array", () => {
-  let testF = (item) => item ** 2;
+  const testF = (item) => item ** 2;
   let x = 1;
-  expect(map(x, testF)).toEqual(new Error(`${x} is not an array`));
+  expect(() => {
+    map(x, testF);
+  }).toThrow(`${x} is not an array`);
 });
+
 test("send an array of objects", () => {
+  const doublePrice = (product) => product.price * 2;
+
   let products = [
     { name: "tomato", price: 20 },
     { name: "grapes", price: 30 },
     { name: "potato", price: 45 },
   ];
-  let doublePrice = (product) => product.price * 2;
+
   expect(map(products, doublePrice)).toEqual([40, 60, 90]);
 });
 
-test("send empty array with a functions", () => {
-  let testFn = function () {};
+test("send empty array with a function", () => {
+  const testFn = (item) => item ** 2;
   expect(map([], testFn)).toEqual([]);
 });
